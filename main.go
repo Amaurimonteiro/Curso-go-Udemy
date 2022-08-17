287eed2ea08bb4a5b9819c49700feb80e9f64fc1
Amauri Soares Monteiro <leidincomp@gmail.com>
2022-08-17 12:37:06 -0300

    a1

--- main.go
+++ main.go
@@ -11,4 +11,5 @@ func main() {
 	// teste
 	// Mais uma alteração
 	// mais uma
+	
 }
--- matematica/matematica.go
+++ matematica/matematica.go
@@ -1,6 +1,7 @@
 package matematica
 
-// Soma devolve a soma de dois numeros
+//Soma devolve a soma de dois numeros
 func Soma(n1 int, n2 int) {
-	return (n1 + n2)
+	return n1 + n2
+
 }
